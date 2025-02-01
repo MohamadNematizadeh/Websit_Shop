6c1c3d5f9289a946d2121379dce51baeb1788df9
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2025-02-01 23:37:16 +0330

    Update ProductForm.tsx

--- src/app/components/ProductForm.tsx
+++ src/app/components/ProductForm.tsx
@@ -17,7 +17,7 @@ export default function AddProductForm({ onAdd }: { onAdd: (newProduct: Product)
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/products", {
+      const response = await fetch("https://backend-api.liara.run/api/products", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ id: Date.now(), name, price: Number(price), category, image }),
