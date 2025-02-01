9909f15df62fb9dbd530e52d7a27cd26558986f2
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2025-02-01 22:57:41 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AddProductForm from "./components/ProductForm";
 import ProductList from "./components/ProductList";
-import Image from 'next/image';
+import Image from "next/image";
 
 interface Product {
   id: number;
@@ -30,6 +30,7 @@ export default function Home() {
       console.error("Error fetching products:", error);
     }
   };
+
   const handleDelete = async (id: number) => {
     try {
       const response = await fetch(`http://127.0.0.1:8000/api/products/${id}`, {
@@ -39,7 +40,9 @@ export default function Home() {
       if (!response.ok) {
         throw new Error("Failed to delete product");
       }
-      setProducts((prevProducts) => prevProducts.filter((product) => product.id !== id));
+      setProducts((prevProducts) =>
+        prevProducts.filter((product) => product.id !== id)
+      );
     } catch (error) {
       console.error("Error deleting product:", error);
     }
@@ -65,34 +68,60 @@ export default function Home() {
           height={399}
         />
         <div className="main-text-box animate-slideIn font-iransans">
-          <h1><span className="highlight">باخشکبار</span>; خشکبار و آجیل</h1>
+          <h1>
+            <span className="highlight">باخشکبار</span>; خشکبار و آجیل
+          </h1>
           <span className="custom-text-before">BAKHOSHKBAR</span>
         </div>
 
         <div className="category-section font-iransans">
           <div className="category-box flex flex-col items-center space-y-2 bg-gray-30 p-4 rounded-lg">
-            <Image src="public\image\آیکن-بادام-زمینی111-copy.png" alt="آیکن بادام زمینی" className="w-8 h-8" />
+            <Image
+              src="/image/آیکن-بادام-زمینی111-copy.png"
+              alt="آیکن بادام زمینی"
+              className="w-8 h-8"
+              width={32}
+              height={32}
+            />
             <span className="text-lg font-semibold">آجیل</span>
           </div>
           <div className="category-box flex flex-col items-center space-y-4 bg-gray-40 p-4 rounded-lg">
-            <Image src="public\image\آیکن-مویز2-copy.png" alt="آیکن بادام زمینی" className="w-8 h-8" />
+            <Image
+              src="/image/آیکن-مویز2-copy.png"
+              alt="آیکن مویز"
+              className="w-8 h-8"
+              width={32}
+              height={32}
+            />
             <span className="text-lg font-semibold">خشکبار</span>
           </div>
           <div className="category-box flex flex-col items-center space-y-2 bg-gray-30 p-4 rounded-lg">
-            <Image src="public\image\آیکن-بادام-درختی2-1.png" alt="آیکن بادام زمینی" className="w-8 h-8" />
+            <Image
+              src="/image/آیکن-بادام-درختی2-1.png"
+              alt="آیکن بادام درختی"
+              className="w-8 h-8"
+              width={32}
+              height={32}
+            />
             <span className="text-lg font-semibold">بادام</span>
           </div>
           <div className="category-box flex flex-col items-center space-y-2 bg-gray-30 p-4 rounded-lg">
-            <img src="public\image\آیکن-پسته2-copy.png" alt="آیکن بادام زمینی" className="w-8 h-8" />
+            <Image
+              src="/image/آیکن-پسته2-copy.png"
+              alt="آیکن پسته"
+              className="w-8 h-8"
+              width={32}
+              height={32}
+            />
             <span className="text-lg font-semibold">پسته</span>
           </div>
         </div>
       </section>
       <br />
-      
+
       <ProductList products={products} onDelete={handleDelete} />
       <AddProductForm onAdd={handleAddProduct} />
-      
+
       <Footer />
     </div>
   );
