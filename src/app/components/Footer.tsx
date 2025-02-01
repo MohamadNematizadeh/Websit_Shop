3d8ef63fc3e6e86913e8e843ab95ffbe220672fe
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2025-02-01 22:57:15 +0330

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,12 +1,13 @@
-// components/Footer.tsx
-`use client`
+"use client";
+
 export default function Footer() {
-    return (
-        <footer className="bg-gray-900 text-white p-2">
-            <div className="text-center mt-2">
-                <p className="text-sm text-gray-400">طراحی سایت توسط محمد نعمتی زاده | © 2025</p>
-            </div>
-        </footer>
-    );
-  }
-  
+  return (
+    <footer className="bg-gray-900 text-white p-2">
+      <div className="text-center mt-2">
+        <p className="text-sm text-gray-400">
+          طراحی سایت توسط محمد نعمتی زاده | © 2025
+        </p>
+      </div>
+    </footer>
+  );
+}
